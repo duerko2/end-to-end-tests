ce3a99af2335c170c5dac31b6f946b394e34ee99
marcusjacobsen <S215799@dtu.dk>
2024-01-16 15:04:36 +0100

    fixings

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
+
 docker image prune -f
+
 docker-compose up -d rabbitMq
-sleep 20
+
+sleep 15
+
 docker-compose up -d account-service token-service payment-service bank-service
+
+
 
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
 mvn clean test 
+docker image prune -f
